c759af972f2befb038e3bcdf97648184688c3d2a
Ruddy Janpierts Correa Grillo <30013868+janpierts@users.noreply.github.com>
2025-12-14 20:45:42 -0500

    Programming Basics - Booleans  / Exercise 2

--- Programming Basics/Booleans/Exercise 2/src/Task.kt
+++ Programming Basics/Booleans/Exercise 2/src/Task.kt
@@ -1,12 +1,35 @@
 // Booleans/Task2.kt
 package booleansExercise2
 
-fun showAnd() {}
+fun showAnd(first: Boolean, second: Boolean) {
+  val result = first && second
+  println("$first && $second == $result")
 
-fun showOr() {}
+}
+
+fun showOr(first: Boolean, second: Boolean) {
+  val result = first || second
+  println("$first || $second == $result")
+}
 
 fun showTruthTable() {
-  TODO()
+  val values = arrayOf(true, false)
+
+  println("Truth Table for AND (&&):")
+  for (first in values) {
+    for (second in values) {
+      showAnd(first, second)
+    }
+  }
+
+  println()
+
+  println("Truth Table for OR (||):")
+  for (first in values) {
+    for (second in values) {
+      showOr(first, second)
+    }
+  }
 }
 
 fun main() {
